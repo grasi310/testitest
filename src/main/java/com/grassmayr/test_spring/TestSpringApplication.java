adef84848ab4516cb71949ef6b1b7ddbddea3407
Grasi310 <m.grassmayr@gmx.net>
2018-12-17 12:05:59 +0100

    Zwoater Commit

--- src/main/java/com/grassmayr/test_spring/TestSpringApplication.java
+++ src/main/java/com/grassmayr/test_spring/TestSpringApplication.java
@@ -1,14 +1,22 @@
 package com.grassmayr.test_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
-public class TestSpringApplication {
+public class TestSpringApplication implements CommandLineRunner {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello nutte");
+	}
 }
 
